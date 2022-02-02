958275ec26a118799940d0a8da8713aab325fc32
Leticijak <mnogomijeheavy@gmail.com>
2022-02-02 12:26:55 +0100

    donzo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react"
+import { imgInfo } from "./data"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div></div>
+  return (
+    <>
+      <div className='card-wrapper'>
+        {imgInfo.map((img) => (
+          <div className='img-wrap' key={img.id}>
+            <button>view</button>
+            <img className='img-img' src={img.url} width={250} alt='image' />
+            <div className='card-body'>
+              <h1>{img.title}</h1>
+              <p>{img.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/data.js
@@ -0,0 +1,65 @@
+export const imgInfo = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1591824438708-ce405f36ba3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1574870111867-089730e5a72b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1476922027627-aa7293e3aaa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1570527140771-020891229bb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 5,
+    url: "https://images.unsplash.com/photo-1629812456605-4a044aa38fbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 6,
+    url: "https://images.unsplash.com/photo-1609695001873-bf16717ba9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fGFuaW1hbHN8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 7,
+    url: "https://images.unsplash.com/photo-1615871155271-64f7c939833b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 8,
+    url: "https://images.unsplash.com/photo-1503256207526-0d5d80fa2f47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+  {
+    id: 9,
+    url: "https://images.unsplash.com/photo-1591382386627-349b692688ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe impedit alias voluptatem",
+  },
+]
